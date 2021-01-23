8d1729cb6d83b38521f770a097efd2b8754c9b26
zed <zshake11@gmail.com>
2021-01-23 12:59:48 +0530

    folder structure created. swiper.js added

---
+++ src/Homepage/Homepage.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Homepage = () => {
+    return (
+        <section className="homepage">
+            <div className="container-fluid">
+
+                {/* TODAYS SPECIAL */}
+                <div className="todays_special p-3 ps-0">
+                    <h6 className="fw-bold">TODAYS SPECIALS</h6>
+
+                </div>
+
+                {/* TOP SELLING */}
+
+                {/* ALL DISHES WITH PAGINATION & SEARCH*/}
+
+                {/* FILTER POPUP */}
+
+            </div>
+        </section>
+    )
+}
+
+export default Homepage
---
+++ src/ProductDetail/Details.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Details = () => {
+    return (
+        <section className="product_details">
+            <h1>details</h1>
+        </section>
+    )
+}
+
+export default Details
